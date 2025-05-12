e9ec0fd51b911826f5836068c141d8e19888cda9
Ankit Kumar <ankitkumar13122003@gmail.com>
2025-05-12 23:07:22 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, redirect, render_template
+
+# Application Instance : Flask Object
+app = Flask(__name__)
+
+
+# Homepage
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+# Contact Us
+@app.route('/contact-us')
+def contact_us():
+    return render_template("contact-us.html")
+
+# TRP Service :-
+@app.route('/trp-service')
+def trp_service():
+    return render_template("trp-service.html")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
