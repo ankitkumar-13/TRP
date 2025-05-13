d4967cb14c8e6f6122eb09d63f3aeb7c7fdec77e
Ankit Kumar <ankitkumar13122003@gmail.com>
2025-05-14 00:07:42 +0530

    Optimized Removals and Raw Data Addition
    Added raw data for cities and location.
    Managed the Location and City class to handle data.
    Additional JS Handles for ajax requests. to bring out content.
    Loading image shown for unavailable pictures.
    Add (error.html) but is unusable.

--- database.py
+++ database.py
@@ -1,6 +1,17 @@
+from raw_data_storage import raw_data
+
 class Location:
     def __init__(self, name, description, rating, image_path):
         self.name = name
         self.description = description
         self.rating =  rating
         self.image_path = image_path
+class City:
+    def __init__(self, name, description, locations):
+        self.name = name
+        self.description = description
+        self.locations = locations
+
+#Database :-
+cities = [City(city[0], city[1], [Location(loc[0], loc[1], loc[2], loc[3]) for loc in city[2]])
+                    for city in raw_data]
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, redirect, render_template
-from database import Location
+from flask import Flask, redirect, request, render_template
+from database import cities
 
 # Application Instance : Flask Object
 app = Flask(__name__)
@@ -15,17 +15,34 @@ def home():
 def contact_us():
     return render_template("contact-us.html")
 
+@app.route('/get-city-desc', methods=['POST'])
+def get_city_desc():
+    city = request.form.get("city")
+    desc = "No Option Selected"
+    for c in cities:
+        if(c.name.lower() == city.lower()):
+            desc = c.description
+            break
+    return desc
+
+
 @app.route('/get-city-locations', methods=['POST'])
 def get_location_info():
-    lc = Location("GEHU", "An Institute", 0, None)
-    locations = [lc, ]
-    return render_template("location-sub-template.html", locations = locations)
+    city = request.form.get("city")
+    for c in cities:
+        if(c.name.lower() == city.lower()):
+            return render_template("location-sub-template.html", locations = c.locations)
+    return redirect("error", 404)
+
+@app.route('/error')
+def error():
+    return render_template("error.html")
 
 # TRP Service :-
 @app.route('/trp-service')
 def trp_service():
-    cities = ["Dehradun", "Agra", "Delhi"]
-    return render_template("trp-service.html", cities = cities)
+    cities_names = [c.name for c in cities]
+    return render_template("trp-service.html", cities = cities_names)
 
 
 
